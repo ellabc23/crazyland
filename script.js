7e05ee21c3b2e148b021c8dad56dd5d542947d38
Ella <33379153+ellabc23@users.noreply.github.com>
2018-12-01 11:00:33 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function login() {
+var username = document.getElementById('username').value;
+var password =  document.getElementById('password').value;
+if ((username=='ellabc23'&& password=='lucaslove143')||(username=='mom' && password=='mom')||(username=='dad'&& password=='dad')||(username=='Lucas'&& password=='Lucas')||(username=='guest'&& password=='crazyguest')||(username=='NinaChen'&& password=='whatislove')||(username=='HanaShukairy'&& password=='Hanarocks')||(username=='JocelynKwan'&& password=='soccer')){
+window.location.href='index.html';
+}
+} 
+
+
+
+ 
