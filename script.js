53fa32e5dbe8a52a80acb4d7af368367f07d7958
Ella <33379153+ellabc23@users.noreply.github.com>
2018-12-08 10:21:51 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function login() {
 var username = document.getElementById('username').value;
 var password =  document.getElementById('password').value;
-if ((username=='ellabc23'&& password=='lucaslove143')||(username=='mom' && password=='mom')||(username=='dad'&& password=='dad')||(username=='Lucas'&& password=='Lucas')||(username=='guest'&& password=='crazyguest')||(username=='NinaChen'&& password=='whatislove')||(username=='HanaShukairy'&& password=='Hanarocks')||(username=='JocelynKwan'&& password=='soccer')){
-window.location.href='index.html';
+if ((username=='ellabc23'&& password=='lucaslove143')||(username=='mom' && password=='mom')||(username=='dad'&& password=='dad')||(username=='MirrorLeafCup'&& password=='AaAaAaAaAa')||(username=='guest'&& password=='crazyguest')||(username=='NinaChen'&& password=='yes or yes')||(username=='FredSlothigan'&& password=='hanarocks1')||(username=='JocelynKwan'&& password=='soccer')||(username=='kabla'&& password=='yuing101010')||(username=='Flower'&& password=='Zahara')){
+window.location.href='index.mhtl';
 }
 } 
 
