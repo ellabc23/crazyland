6dc320b3ed28e38fb17bc525c51bf41365e8cad1
Ella <33379153+ellabc23@users.noreply.github.com>
2018-12-09 16:24:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login() {
 var username = document.getElementById('username').value;
 var password =  document.getElementById('password').value;
 if ((username=='ellabc23'&& password=='lucaslove143')||(username=='mom' && password=='mom')||(username=='dad'&& password=='dad')||(username=='MirrorLeafCup'&& password=='AaAaAaAaAa')||(username=='guest'&& password=='crazyguest')||(username=='NinaChen'&& password=='yes or yes')||(username=='FredSlothigan'&& password=='hanarocks1')||(username=='JocelynKwan'&& password=='soccer')||(username=='kabla'&& password=='yuing101010')||(username=='Flower'&& password=='Zahara')){
-window.location.href='index.mhtl';
+window.location.href='/crazyland';
 }
 } 
 
