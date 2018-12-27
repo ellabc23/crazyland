3303dd963e1d6a60d335c92ee3f69dde9555fccf
Ella <33379153+ellabc23@users.noreply.github.com>
2018-12-26 17:11:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function login() {
 var username = document.getElementById('username').value;
 var password =  document.getElementById('password').value;
-if ((username=='ellabc23'&& password=='lucaslove143')||(username=='mbialonchoy' && password=='ellal0ve')||(username=='yourusername'&& password=='yourpassword')||(username=='MirrorLeafCup'&& password=='AaAaAaAaAa')||(username=='guest'&& password=='crazyguest')||(username=='NinaChen'&& password=='yes or yes')||(username=='FredSlothigan'&& password=='hanarocks1')||(username=='JocelynKwan'&& password=='soccer')||(username=='kabla'&& password=='yuing101010')||(username=='flower'&& password=='tree')){
+if ((username=='ellabc23'&& password=='lucaslove143')||(username=='mbialonchoy' && password=='ellal0ve')||(username=='yourusername'&& password=='yourpassword')||(username=='MirrorLeafCup'&& password=='AaAaAaAaAa')||(username=='guest'&& password=='crazyguest')||(username=='NinaChen'&& password=='yes or yes')||(username=='FredSlothigan'&& password=='hanarocks1')||(username=='JocelynKwan'&& password=='soccer')||(username=='kabla'&& password=='yuing101010')||(username=='flower'&& password=='tree')||(username=='PaigeMiller'&& password=='Shhhutup')||)(username=='BrooklynDeAsis'&& password=='imacatwhatdocatssay'){
 window.location.href='/crazyland';
 }
 } 
