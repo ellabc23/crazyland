b18b0c528bf7f13aa3369699ec3954c9d92237da
Ella <33379153+ellabc23@users.noreply.github.com>
2020-06-15 10:43:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function login() {
 var username = document.getElementById('username').value;
 var password =  document.getElementById('password').value;
-if ((username=='ellabc23'&& password=='lucaslove143')||(username=='mbialonchoy' && password=='ellal0ve')||(username=='CrazyKaylee' && password=='frodopup')||(username=='yourusername'&& password=='yourpassword')||(username=='MirrorLeafCup'&& password=='AaAaAaAaAa')||(username=='guest'&& password=='crazyguest')||(username=='NinaChen'&& password=='yes or yes')||(username=='FredSlothigan'&& password=='hanarocks1')||(username=='JocelynKwan'&& password=='soccer')||(username=='kabla'&& password=='yuing101010')||(username=='flower'&& password=='tree')||(username=='PaigeMiller'&& password=='Shhhutup')||(username=='BrooklynDeAsis'&& password=='imacatwhatdocatssay')||(username=='EmmaEscobar'&& password=='forwardBackwardInwardOut')){
+if ((username=='ellabc23'&& password=='nutella23')||(username=='mbialonchoy' && password=='ellal0ve')||(username=='CrazyKaylee' && password=='frodopup')||(username=='yourusername'&& password=='yourpassword')||(username=='MirrorLeafCup'&& password=='AaAaAaAaAa')||(username=='guest'&& password=='crazyguest')||(username=='NinaChen'&& password=='yes or yes')||(username=='FredSlothigan'&& password=='hanarocks1')||(username=='JocelynKwan'&& password=='soccer')||(username=='kabla'&& password=='yuing101010')||(username=='flower'&& password=='tree')||(username=='Paige'&& password=='Paige')||(username=='Brooklyn'&& password=='Brooklyn')||(username=='Emma'&& password=='Emma')){
 window.location.href='/crazyland';
 }
 } 
